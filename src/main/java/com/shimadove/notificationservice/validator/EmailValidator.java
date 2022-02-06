1ce824f898c304bce31bf2257ce904f23522766a
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-02-06 10:04:17 +0100

    modified email validator scope. email validator is scope to singleton and can only have on instance running at a time. email validator is also made a component and handed to spring so that it can be created and destroyed with spring framework.

--- src/main/java/com/shimadove/notificationservice/validator/EmailValidator.java
+++ src/main/java/com/shimadove/notificationservice/validator/EmailValidator.java
@@ -1,10 +1,15 @@
 package com.shimadove.notificationservice.validator;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
+
+@Component
+@Scope("Singleton")
 public class EmailValidator {
 
     public boolean isValid(String email, String domainName){
